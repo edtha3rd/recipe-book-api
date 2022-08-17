733b894f7755d33d22165f2222d3fc3d481357af
Tawanda <ejmunongo@gmail.com>
2022-08-17 18:11:45 +0200

    add docker config

---
+++ src/controllers/recipes.controller.ts
@@ -0,0 +1,29 @@
+import { Request, Response, Router } from 'express'
+
+//prisma
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+const router = Router()
+
+async function main() {
+  // add prisma client queries here
+  // router.get('/user', (req: Request, res: Response) => {
+  //   res.send('Hello User')
+  // })
+  router.get('/recipes', async (req: Request, res: Response) => {
+    const recipes = await prisma.user.findMany()
+    console.log(recipes)
+    res.send(recipes)
+  })
+}
+
+main()
+  .catch((e) => {
+    console.log(e.message)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
+
+export default router
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
+import recipesController from '../controllers/recipes.controller'
 import usersController from '../controllers/users.controller'
 
-const api = Router().use(usersController)
+const api = Router().use(usersController).use(recipesController)
 
 export default Router().use('/', api)

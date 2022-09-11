8e1126fda840e573331a9a071d513f03fae45c77
Tawanda <ejmunongo@gmail.com>
2022-09-11 08:04:04 +0200

    update callback URL for Google Auth

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -12,7 +12,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:8008/auth/google/callback/",
+      callbackURL: `${process.env.API}/auth/google/callback/`,
       scope: [" profile "],
       state: true,
     },

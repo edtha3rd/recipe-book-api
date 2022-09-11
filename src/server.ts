df94b5048b54b22b2321136a9e3e36e6856dd334
Tawanda <ejmunongo@gmail.com>
2022-09-11 08:05:22 +0200

    update CORS allowedOrigins

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,10 @@ import routes from "./routes/routes";
 
 // Add a list of allowed origins.
 // If you have more origins you would like to add, you can add them to the array below.
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://letsfeast.netlify.app/",
+];
 
 const options: cors.CorsOptions = {
   origin: allowedOrigins,

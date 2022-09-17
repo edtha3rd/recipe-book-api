a164c7d7164f9e797e318b94b0eb28a06b7e38f9
Tawanda <ejmunongo@gmail.com>
2022-09-17 10:18:10 +0200

    update allowed origins

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ var MongoDBStore = require("connect-mongodb-session")(session);
 
 // Add a list of allowed origins.
 // If you have more origins you would like to add, you can add them to the array below.
-const allowedOrigins = [process.env.CLIENT_URL];
+const allowedOrigins = [process.env.CLIENT_URL, process.env.API];
 
 const options: cors.CorsOptions = {
   origin: allowedOrigins,
